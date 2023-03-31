ab72ba811fe7ef1549b4ef64a949a2ba824b1cd7
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2023-03-31 20:20:42 -0300

    feat: add food points to queen

--- queen/Queen.gd
+++ queen/Queen.gd
@@ -2,5 +2,21 @@ class_name Queen
 extends Node3D
 
 
-func _on_lifespan_timeout() -> void:
+@export var _food_points : int:
+	set(value):
+		_food_points = value
+
+		if _food_points <= 0:
+			_die()
+
+
+func _on_foodspan_timeout() -> void:
+	_die()
+
+
+func _on_food_points_timer_timeout() -> void:
+		_food_points -= 1
+
+
+func _die() -> void:
 	queue_free()
