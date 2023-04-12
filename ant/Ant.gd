367d96f351a18ff2820f15acba57f049053342f3
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2023-04-12 14:31:26 -0300

    feat: implement ant movement

--- ant/Ant.gd
+++ ant/Ant.gd
@@ -1,5 +1,12 @@
 class_name Ant
-extends Node3D
+extends CharacterBody3D
+
+const _ROTATION_ANGLE: float = PI / 2
+
+const _CLIMB_ANGLE: float = PI / 2
+
+const _CLIMB_OFFSET: float = -0.2
+const _DESCENT_OFFSET: float = 0
 
 
 @export var _food_points: int:
@@ -8,6 +15,59 @@ extends Node3D
 
 		if _food_points <= 0:
 			_die()
+
+
+@export var _movement_speed: float
+
+@onready var _wall_sensor: RayCast3D = $WallSensor
+@onready var _ledge_sensor: RayCast3D = $LedgeSensor
+
+func _physics_process(_delta: float) -> void:
+	_handle_movement()
+	_handle_rotation()
+	_handle_climbing()
+
+
+func _handle_movement() -> void:
+	if not Input.is_action_pressed("move_ant_forward"):
+		return
+
+	velocity = -basis.z * _movement_speed
+	move_and_slide()
+
+
+func _handle_rotation() -> void:
+	var rotation_direction := 0
+
+	if Input.is_action_just_pressed("rotate_ant_left"):
+		rotation_direction = 1
+	elif Input.is_action_just_pressed("rotate_ant_right"):
+		rotation_direction = -1
+
+	if rotation_direction != 0:
+		rotate(basis.y, rotation_direction * _ROTATION_ANGLE)
+
+
+func _handle_climbing() -> void:
+	var surface: RayCast3D = null
+	var direction: float = 0
+	var offset: float = 0
+
+	if _wall_sensor.is_colliding():
+		surface = _wall_sensor
+		direction = 1
+		offset = _CLIMB_OFFSET
+		print("wall")
+	elif _ledge_sensor.is_colliding():
+		surface = _ledge_sensor
+		direction = -1
+		offset = _DESCENT_OFFSET
+		print("ledge")
+	else:
+		return
+
+	rotate(basis.x, direction * _CLIMB_ANGLE)
+	position = surface.get_collision_point() + offset * basis.z
 
 
 func _on_lifespan_timeout() -> void:
