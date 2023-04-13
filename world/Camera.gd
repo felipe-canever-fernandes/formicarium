003e984dc4685c21e1735c2ec7374b23e677f30f
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2023-04-13 18:47:10 -0300

    refactor: apply GDScript style guide to camera code

--- world/Camera.gd
+++ world/Camera.gd
@@ -1,12 +1,12 @@
 extends Camera3D
 
-const MINIMUM_X_ROTATION: float = -90
-const MAXIMUM_X_ROTATION: float = 90
+const _MINIMUM_X_ROTATION: float = -90
+const _MAXIMUM_X_ROTATION: float = 90
 
-@export var _movement_speed: float
-@export var _rotation_speed: float
+@export var _movement_speed: float = 0
+@export var _rotation_speed: float = 0
 
-var _mouse_motion_delta := Vector2.ZERO
+var _mouse_motion_delta: Vector2 = Vector2.ZERO
 
 
 func _process(delta: float) -> void:
@@ -15,7 +15,7 @@ func _process(delta: float) -> void:
 
 
 func _handle_movement(delta: float) -> void:
-	var direction := Vector3.ZERO
+	var direction: Vector3 = Vector3.ZERO
 
 	if Input.is_action_pressed("forward"):
 		direction.z += -1
@@ -35,7 +35,7 @@ func _handle_movement(delta: float) -> void:
 	if Input.is_action_pressed("strafe_up"):
 		direction.y += 1
 
-	var velocity := direction * _movement_speed * delta
+	var velocity: Vector3 = direction * _movement_speed * delta
 	translate_object_local(velocity)
 
 
@@ -43,9 +43,9 @@ func _handle_rotation(delta: float) -> void:
 	_mouse_motion_delta *= _rotation_speed * delta
 
 	rotation_degrees.x = clamp(
-		rotation_degrees.x + _mouse_motion_delta.y,
-		MINIMUM_X_ROTATION,
-		MAXIMUM_X_ROTATION,
+			rotation_degrees.x + _mouse_motion_delta.y,
+			_MINIMUM_X_ROTATION,
+			_MAXIMUM_X_ROTATION,
 	)
 
 	rotation_degrees.y += _mouse_motion_delta.x
@@ -63,7 +63,9 @@ func _input(event: InputEvent) -> void:
 
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-	var input_event_mouse_motion := event as InputEventMouseMotion
+	var input_event_mouse_motion: InputEventMouseMotion = (
+			event as InputEventMouseMotion
+	)
 
 	_mouse_motion_delta.x -= input_event_mouse_motion.relative.x
 	_mouse_motion_delta.y -= input_event_mouse_motion.relative.y
