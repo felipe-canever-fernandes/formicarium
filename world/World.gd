d3594b23f0afa6951f4f1184126c77f4f216d386
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2023-04-13 18:50:12 -0300

    refactor: apply GDScript style guide to world code

--- world/World.gd
+++ world/World.gd
@@ -2,9 +2,9 @@ extends Node3D
 
 const _RAY_LENGTH: int = 4000
 
-@onready var _fps := $FPS as Label
-@onready var _camera := $Camera as Camera3D
-@onready var _terrain := $Terrain as Terrain
+@onready var _fps: Label = $FPS as Label
+@onready var _camera: Camera3D = $Camera as Camera3D
+@onready var _terrain: Terrain = $Terrain as Terrain
 
 
 func _process(_delta: float) -> void:
@@ -23,15 +23,24 @@ func _handle_block_interaction(
 	if not Input.is_action_just_released(action):
 		return
 
-	var mouse_position := get_viewport().get_mouse_position()
+	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 
-	var parameters := PhysicsRayQueryParameters3D.new()
+	var parameters: PhysicsRayQueryParameters3D = (
+			PhysicsRayQueryParameters3D.new()
+	)
+
 	parameters.from = _camera.project_ray_origin(mouse_position)
-	parameters.to = parameters.from +\
-			_camera.project_ray_normal(mouse_position) * _RAY_LENGTH
 
-	var space_state := get_world_3d().direct_space_state
-	var result := space_state.intersect_ray(parameters)
+	parameters.to = (
+			parameters.from
+			+ _camera.project_ray_normal(mouse_position) * _RAY_LENGTH
+	)
+
+	var space_state: PhysicsDirectSpaceState3D = (
+			get_world_3d().direct_space_state
+	)
+
+	var result: Dictionary = space_state.intersect_ray(parameters)
 
 	if not result.is_empty():
 		_terrain.call(function_name, result["position"], result["normal"])
